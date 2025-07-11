efa1778a7fb14bf6ceae1464d0ed2fe1d1fb6998
Abdul Rahman Sibahi <asibahi@users.noreply.github.com>
2025-07-12 00:26:19 +0300

    minimize bug

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,10 @@
-use ab_glyph::Font;
+use rustybuzz::ttf_parser::{self, GlyphId, OutlineBuilder};
 
 fn main() {
     let text = "Roboto";
 
     let font = include_bytes!("../RobotoSerif[grad,opsz,wdth,wgth].ttf");
     // let font = include_bytes!("../NotoArabic.ttf"); // works !
-
-    let ab_font = ab_glyph::FontRef::try_from_slice(font).unwrap();
 
     {
         eprintln!("RustyBuzz");
@@ -18,23 +16,34 @@ fn main() {
 
         let rb_r_glyphid = rb_output.glyph_infos()[0].glyph_id; // should be for R
 
-        let ab_glyph_id = ab_glyph::GlyphId(rb_r_glyphid as u16);
-        eprintln!("{}: {:?}", rb_r_glyphid, ab_font.outline(ab_glyph_id));
+        {
+            let ttf_font = ttf_parser::Face::parse(font, 0).unwrap();
+            let mut mock = OutlineMocker;
+            ttf_font.outline_glyph(GlyphId(rb_r_glyphid as u16), &mut mock);
+        }
+    }
+}
+
+struct OutlineMocker;
+
+impl OutlineBuilder for OutlineMocker {
+    fn move_to(&mut self, x: f32, y: f32) {
+        eprintln!("move_to {x}, {y}");
     }
 
-    {
-        eprintln!("HarfBuzz");
+    fn line_to(&mut self, x: f32, y: f32) {
+        eprintln!("line_to {x}, {y}");
+    }
 
-        let hb_font = harfbuzz_rs::Face::from_bytes(font, 0);
-        let hb_font = harfbuzz_rs::Font::new(hb_font);
-        let buf = harfbuzz_rs::UnicodeBuffer::new().add_str(text);
-        let hb_output = harfbuzz_rs::shape(&hb_font, buf, &[]);
+    fn quad_to(&mut self, x1: f32, y1: f32, x: f32, y: f32) {
+        eprintln!("quad_to {x1}, {y1}, {x}, {y}");
+    }
 
-        let hb_r_glyphid = hb_output.get_glyph_infos()[0].codepoint; // should be for R
+    fn curve_to(&mut self, x1: f32, y1: f32, x2: f32, y2: f32, x: f32, y: f32) {
+        eprintln!("curv_to {x1}, {y1}, {x2}, {y2}, {x}, {y}");
+    }
 
-        let ab_glyph_id = ab_glyph::GlyphId(hb_r_glyphid as u16);
-        eprintln!("{}: {:?}", hb_r_glyphid, ab_font.outline(ab_glyph_id));
-    };
-
-    // println!("Hello, world!");
+    fn close(&mut self) {
+        eprintln!("close");
+    }
 }
