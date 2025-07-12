2b60663147ed0ceafaabcbd5b3f6af6f5d486f66
Abdul Rahman Sibahi <asibahi@users.noreply.github.com>
2025-07-12 14:30:37 +0300

    add skrifa

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use rustybuzz::ttf_parser::{self, GlyphId, OutlineBuilder};
+use skrifa::{MetadataProvider, outline::OutlinePen, prelude::Size};
 
 fn main() {
     let text = "Roboto";
@@ -7,19 +8,29 @@ fn main() {
     // let font = include_bytes!("../NotoArabic.ttf"); // works !
 
     {
-        eprintln!("RustyBuzz");
-
         let rb_font = rustybuzz::Face::from_slice(font, 0).unwrap();
         let mut buf = rustybuzz::UnicodeBuffer::new();
         buf.push_str(text);
         let rb_output = rustybuzz::shape(&rb_font, &[], buf);
 
         let rb_r_glyphid = rb_output.glyph_infos()[0].glyph_id; // should be for R
+        let mut mock = OutlineMocker;
 
         {
+            eprintln!("ttf_parser");
             let ttf_font = ttf_parser::Face::parse(font, 0).unwrap();
-            let mut mock = OutlineMocker;
-            ttf_font.outline_glyph(GlyphId(rb_r_glyphid as u16), &mut mock);
+            let res = ttf_font.outline_glyph(GlyphId(rb_r_glyphid as u16), &mut mock);
+            dbg!(res.is_some());
+        }
+        {
+            eprintln!("skrifa"); // works
+            let sk_font = skrifa::FontRef::new(font).unwrap();
+            let sk_outlines = sk_font
+                .outline_glyphs()
+                .get(skrifa::GlyphId::new(rb_r_glyphid))
+                .unwrap();
+            let res = sk_outlines.draw(Size::unscaled(), &mut mock);
+            dbg!(res.is_ok());
         }
     }
 }
@@ -41,6 +52,28 @@ impl OutlineBuilder for OutlineMocker {
 
     fn curve_to(&mut self, x1: f32, y1: f32, x2: f32, y2: f32, x: f32, y: f32) {
         eprintln!("curv_to {x1}, {y1}, {x2}, {y2}, {x}, {y}");
+    }
+
+    fn close(&mut self) {
+        eprintln!("close");
+    }
+}
+
+impl OutlinePen for OutlineMocker {
+    fn move_to(&mut self, x: f32, y: f32) {
+        eprintln!("move_to {x}, {y}");
+    }
+
+    fn line_to(&mut self, x: f32, y: f32) {
+        eprintln!("line_to {x}, {y}");
+    }
+
+    fn quad_to(&mut self, cx0: f32, cy0: f32, x: f32, y: f32) {
+        eprintln!("quad_to {cx0}, {cy0}, {x}, {y}");
+    }
+
+    fn curve_to(&mut self, cx0: f32, cy0: f32, cx1: f32, cy1: f32, x: f32, y: f32) {
+        eprintln!("curv_to {cx0}, {cy0}, {cx1}, {cy1}, {x}, {y}");
     }
 
     fn close(&mut self) {
